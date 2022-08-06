6f4ec052a5f33799254c220bb70bf190359edafa
Mike Claros <mike.s.claros@gmail.com>
2022-08-05 20:34:46 -0700

    added operator buttons

--- src/components/CalcBody.jsx
+++ src/components/CalcBody.jsx
@@ -1,35 +1,78 @@
-import React, { useState } from 'react';
-import { CalcButton } from './CalcButton';
+import React, { useEffect, useState } from 'react';
 import { CalcDisplay } from './CalcDisplay';
-
-
 
 export function CalcBody() {
     const [number, setNumber] = useState("")
 
+    // function displayButtons() {
+    //     let btnClassName = 'btn defaultColor rountBtn'
+    //     let operators = ["/", "X", "-", "+"]
+    //     let numbers = 9
+    //     let map = new Map()
+    //     //map buttons to their position in the array
+    //     for (let i = 0; i < maxInRow; i++) {
+    //         map(<button className={btnClassName} onClick={() => setNumber((val) => val + i.toString())}>{val}</button>)
+    //     }
+
+    //     return arr
+    // }
+
+    const set = {
+        zero: () => setNumber((num) => num + "0"),
+        one: () => setNumber((num) => num + "1"),
+        two: () => setNumber((num) => num + "2"),
+        three: () => setNumber((num) => num + "3"),
+        four: () => setNumber((num) => num + "4"),
+        five: () => setNumber((num) => num + "5"),
+        six: () => setNumber((num) => num + "6"),
+        seven: () => setNumber((num) => num + "7"),
+        eight: () => setNumber((num) => num + "8"),
+        nine: () => setNumber((num) => num + "9"),
+        clear: () => setNumber((num) => ""),
+        divide: () => setNumber((num) => num + "/"),
+        multiply: () => setNumber((num) => num + "X"),
+        subtract: () => setNumber((num) => num + "-"),
+        add: () => setNumber((num) => num + "+"),
+        enter: () => setNumber((num) => num + "="),
+        point: () => setNumber((num) => num + "."),
+    }
+
+
     return (
         <div>
             <div>
-                <CalcDisplay value={number} />
+                <div>
+                    <CalcDisplay value={number} />
+                </div>
+                <div>
+                    <button className='btn defaultColor roundBtn' onClick={set.clear}>C</button>
+                </div>
+                <div>
+                    <button className='btn defaultColor roundBtn' onClick={set.seven}>7</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.eight}>8</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.nine}>9</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.multiply}>X</button>
+                </div>
+                <div>
+                    <button className='btn defaultColor roundBtn' onClick={set.four}>4</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.five}>5</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.six}>6</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.subtract}>-</button>
+                </div>
+                <div>
+                    <button className='btn defaultColor roundBtn' onClick={set.one}>1</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.two}>2</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.three}>3</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.divide}>/</button>
+                </div>
+                <div>
+                    <button className='btn defaultColor roundBtn' onClick={set.zero}>0</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.point}>.</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.plus}>+</button>
+                    <button className='btn defaultColor roundBtn' onClick={set.enter}>=</button>
+                    {/* <button className='btn defaultColor rountBtn' onCl */}
+                </div>
             </div>
-            <div>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "7")}>7</button>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "8")}>8</button>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "9")}>9</button>
-            </div>
-            <div>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "4")}>4</button>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "5")}>5</button>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "6")}>6</button>
-            </div>
-            <div>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "1")}>1</button>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "2")}>2</button>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "3")}>3</button>
-            </div>
-            <div>
-                <button className='btn defaultColor roundBtn' onClick={() => setNumber((num) => num + "0")}>0</button>
-            </div>
-        </div>
+        </div >
     )
 }
