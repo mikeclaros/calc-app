4773326048c3790545a29a9bfd451085e38d6216
Mike Claros <mike.s.claros@gmail.com>
2022-12-02 18:36:17 -0800

    added history title

--- src/components/HistoryDisplay.jsx
+++ src/components/HistoryDisplay.jsx
@@ -7,6 +7,7 @@ export function HistoryDisplay({ value }) {
     return (
         <div>
             <h1 className='history-window'>
+                <span style={{ fontFamily: "consolas", fontSize: "1em" }}>History</span>
                 {console.log("HistoryList: ", historyList)} {console.log("localStorage: ", localStorage.getItem('historyList'))}
                 {(historyList.length < 1) ? console.log('empty historyList') : historyList.map((data, index) => <li className='bullet-less' key={index + data}>{data}</li>)}
             </h1>
