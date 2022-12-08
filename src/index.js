ec401205bc8a28e7e1f35e49ae6f7527e81c1ead
Mike Claros <mike.s.claros@gmail.com>
2022-12-07 18:23:56 -0800

    quick fix removed debug log line

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { CalcBody } from './components/CalcBody';
 
 // disable logs in prod
-console.log("what is node env?: " + process.env.NODE_ENV)
 if (process.env.NODE_ENV === "production") {
     console.log = () => { }
 }
